d77d72c72d09a3a1a531ca0e1d97c128471684b2
marble703 <703342335@qq.com>
2026-02-12 19:52:09 +0800

    feat(example): 测试自动获取状态

---
+++ example/src/reader_get_can.cpp
@@ -0,0 +1,72 @@
+#include "can.hpp"
+#include "interface.hpp"
+#include "logger.hpp"
+
+int main() {
+    auto logger = qdriver::logger::LoggerFactory::createCombinedLogger(
+        "qdrive_reader_can",
+        "qdrive_reader_can.log",
+        spdlog::level::info
+    );
+
+    std::shared_ptr<qdriver::io::Can> canBusPtr = std::make_shared<qdriver::io::Can>("can0");
+
+    qdriver::interface::Interface interface(canBusPtr);
+
+    if (interface.isPortOpen()) {
+        logger->info("CAN port opened successfully.");
+    } else {
+        logger->error("Failed to open CAN port.");
+        return 0;
+    }
+
+    interface.startReaderThread([logger](std::string& buffer) {
+        auto pos = buffer.find(":");
+        if (pos == std::string::npos) {
+            return;
+        }
+
+        std::string idStr   = buffer.substr(0, pos);
+        std::string dataHex = buffer.substr(pos + 1);
+
+        int id = 0;
+        try {
+            id = std::stoi(idStr);
+        } catch (...) {
+            return;
+        }
+
+        std::size_t dlc = dataHex.size() / 2; // 每两个字符是 1 字节
+
+        // 输出格式参考 candump ：can0  400   [3]  00 00 00
+        std::ostringstream oss;
+        oss << "can0  " << std::uppercase << std::hex << id << "   [" << std::dec << dlc
+            << "]  ";
+
+        for (std::size_t i = 0; i + 1 < dataHex.size(); i += 2) {
+            std::string byteStr  = dataHex.substr(i, 2);
+            unsigned int byteVal = 0;
+            try {
+                byteVal = std::stoul(byteStr, nullptr, 16);
+            } catch (...) {
+                return;
+            }
+
+            oss << std::setw(2) << std::setfill('0') << std::uppercase << std::hex << byteVal;
+
+            if (i + 2 < dataHex.size()) {
+                oss << ' ';
+            }
+        }
+
+        logger->info("{}", oss.str());
+    });
+
+    // 主线程定时查询状态
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+        interface.status();
+    }
+
+    return 0;
+}
